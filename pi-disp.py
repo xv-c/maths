91fc892e91e98661c8f0f4141bdf86c7f1463a5d
myob <55620465+xv-c@users.noreply.github.com>
2023-12-07 12:07:16 +0000

    rawr

--- pi-disp.py
+++ pi-disp.py
@@ -13,7 +13,7 @@ def calculate_circle_area(N):
 
 # Формирование таблицы с результатами экспериментов, средним и дисперсией
 results = []
-Ns = [10, 100, 1000, 10000, 100000]
+Ns = [100, 200, 500, 1000, 2000, 5000, 10000]
 for N in Ns:
     areas = [calculate_circle_area(N) for _ in range(10)]  # 10 экспериментов для каждого N
     mean_area = np.mean(areas)
@@ -21,7 +21,9 @@ for N in Ns:
     results.append((N, areas, mean_area, variance_area))
 
 # Вывод результатов
-print("N\t\tВычисленные площади\t\tСреднее\t\tДисперсия")
+print("N\t\tВычисленные площади\t\t\t\t\t\t\t\t\t\t\tСреднее\t\tДисперсия")
 for result in results:
     N, areas, mean_area, variance_area = result
-    print(f"{N}\t\t{areas}\t\t{mean_area:.4f}\t\t{variance_area:.4f}")
+    # Форматирование площадей с двумя знаками после запятой
+    areas_formatted = [f"{area:.2f}" for area in areas]
+    print(f"{N}\t\t{areas_formatted}\t\t{mean_area:.2f}\t\t{variance_area:.2f}")
--- pi.py
+++ pi.py
@@ -5,22 +5,18 @@ y = [0]*3
 r = [0]*3
 rr = [0]*3
 
-# Input the coordinates of the center and radius of three circles
 for j in range(3):
     print(f'{j+1}-я окружность (x y r): ')
     x[j], y[j], r[j] = map(float, input().split())
-    # Calculate the squares of the radii - they will be used often
     rr[j] = r[j]**2
 
 n = int(input('Количество историй: '))
 
-# Describe a square around the first circle
 xmin = x[0] - r[0]
 ymin = y[0] - r[0]
 d = r[0] * 2
 m = 0
 
-# In the loop by the number of stories
 for i in range(n):
     # Randomly throw points into the selected square
     xp = random.random() * d + xmin
